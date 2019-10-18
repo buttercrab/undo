9c2c007a9ccb1e2bde46ecaa04fc323844eccfb3
buttercrab <jaeyong0201@gmail.com>
2019-10-18 21:26:17 +0900

    updated init feature

--- main.go
+++ main.go
@@ -3,32 +3,18 @@ package main
 import (
 	"flag"
 	"fmt"
-	"log"
 	"os"
-	"os/exec"
 )
 
 func printInit() {
-	fmt.Println("export UNDO_INIT=true")
-	fmt.Println("mkdir -p ~/.undo")
+	fmt.Print("export UNDO_INIT=true;")
+	fmt.Print("mkdir -p ~/.undo;")
 	os.Exit(0)
 }
 
-func checkInit() (bool, error) {
-	result, err := exec.Command("echo", "$UNDO_INIT").Output()
-	if err != nil {
-		return false, err
-	}
-	return string(result) == "true", nil
-}
-
 func main() {
-	initCheck, err := checkInit()
-	if err != nil {
-		log.Fatal(err)
-	}
 	initHelpMessage := "Inits the undo command by 'eval $(undo --init)'"
-	if !initCheck {
+	if os.Getenv("UNDO_INIT") != "true" {
 		initHelpMessage += "\nYou terminal doesn't seem to be initialized.\nPut 'eval $(undo --init)' in your ~/.bashrc or ~/.zshrc"
 	}
 	initPtr := flag.Bool("init", false, initHelpMessage)
